c14f874941aa274e6f3511e7996a33978488ca13
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2019-11-22 11:54:43 +0900

    change notebook dirs

--- assets/.jupyter/jupyter_notebook_config.py
+++ assets/.jupyter/jupyter_notebook_config.py
@@ -5,7 +5,7 @@ c.KernelManager.shutdown_wait_time = 10.0
 c.FileContentsManager.delete_to_trash = False
 c.NotebookApp.quit_button = False
 c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
-c.NotebookApp.notebook_dir = '/notebooks'
+c.NotebookApp.notebook_dir = '/jupyter'
 
 if 'PASSWORD' in os.environ:
     from notebook.auth import passwd
