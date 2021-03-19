4186005f01dae5ce6f97a58a0cea5cf7c2fdb196
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2021-03-19 22:45:05 +0900

    update jupyter config

--- assets/.jupyter/jupyter_notebook_config.py
+++ assets/.jupyter/jupyter_notebook_config.py
@@ -1,13 +1,13 @@
 import os
-c.NotebookApp.ip = '*'
-c.NotebookApp.allow_remote_access = True
+c.ServerApp.ip = '*'
+c.ServerApp.allow_remote_access = True
 c.KernelManager.shutdown_wait_time = 10.0
 c.FileContentsManager.delete_to_trash = False
-c.NotebookApp.quit_button = False
-c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
-c.NotebookApp.notebook_dir = '/jupyter'
+c.ServerApp.quit_button = False
+c.ServerApp.terminado_settings = {'shell_command': ['/bin/bash']}
+c.ServerApp.notebook_dir = '/jupyter'
 
 if 'PASSWORD' in os.environ:
     from notebook.auth import passwd
-    c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+    c.ServerApp.password = passwd(os.environ['PASSWORD'])
     del os.environ['PASSWORD']
